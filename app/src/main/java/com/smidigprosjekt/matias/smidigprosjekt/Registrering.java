0b1151f914b5aedc05e2839764b794a5aad19879
VegardG <vegardball@gmail.com>
2018-12-13 11:22:25 +0100

    registreringsside er linket til hovedside

--- app/src/main/java/com/smidigprosjekt/matias/smidigprosjekt/Registrering.java
+++ app/src/main/java/com/smidigprosjekt/matias/smidigprosjekt/Registrering.java
@@ -2,6 +2,10 @@ package com.smidigprosjekt.matias.smidigprosjekt;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Registrering extends AppCompatActivity {
 
@@ -9,5 +13,15 @@ public class Registrering extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrering);
+
+        Button registerBtn = (Button)findViewById(R.id.registerBtn);
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent startIntent = new Intent(getApplicationContext(), HomePage.class);
+                startActivity(startIntent);
+            }
+        });
     }
 }
