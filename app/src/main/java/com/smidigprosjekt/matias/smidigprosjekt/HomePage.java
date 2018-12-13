4e75d82856f5bc03f4b9bfc0c2211271f65aa691
CaptainMomo <31258188+CaptainMomo@users.noreply.github.com>
2018-12-13 16:21:40 +0100

    Fungerende Registrering og HomePage

--- app/src/main/java/com/smidigprosjekt/matias/smidigprosjekt/HomePage.java
+++ app/src/main/java/com/smidigprosjekt/matias/smidigprosjekt/HomePage.java
@@ -1,25 +1,10 @@
 package com.smidigprosjekt.matias.smidigprosjekt;
 
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.design.widget.TabLayout;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-
-import android.widget.SectionIndexer;
-import android.widget.TextView;
 
 public class HomePage extends AppCompatActivity {
 
