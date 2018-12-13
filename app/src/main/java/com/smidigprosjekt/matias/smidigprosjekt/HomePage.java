78f8cf9675d3caba9773182487cd404ecb15067a
CaptainMomon <dalmat17@student.westerdals.no>
2018-12-13 18:34:36 +0100

    Navigasjonsbar nederst
    uten funksjon foreløpig

--- app/src/main/java/com/smidigprosjekt/matias/smidigprosjekt/HomePage.java
+++ app/src/main/java/com/smidigprosjekt/matias/smidigprosjekt/HomePage.java
@@ -1,10 +1,15 @@
 package com.smidigprosjekt.matias.smidigprosjekt;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.MenuItem;
 
 public class HomePage extends AppCompatActivity {
 
@@ -29,7 +34,37 @@ public class HomePage extends AppCompatActivity {
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(mViewPager);
 
+        /*BottomNavigationView navigationView = findViewById(R.id.bottom_nav);
+
+        final GroupFragment groupFragment = new GroupFragment();
+        final ChatFragment chatFragment = new ChatFragment();
+        final NotificationFragment notificationFragment = new NotificationFragment();
+
+        navigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                int id = menuItem.getItemId();
+                if(id == R.id.group) {
+                    setFragment(groupFragment);
+                    return true;
+                } else if (id == R.id.chat){
+                    setFragment(chatFragment);
+                    return true;
+                } else if (id == R.id.notifications){
+                    setFragment(notificationFragment);
+                    return true;
+                }
+                return false;
+            }
+        });*/
+
+
     }
+    /*private void setFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.frame, fragment);
+        fragmentTransaction.commit();
+    }*/
 
     private void setupViewPager(ViewPager viewPager) {
         SectionsPageAdapter adapter = new SectionsPageAdapter(getSupportFragmentManager());
